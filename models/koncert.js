356b046e45baffca64ac0e4291d9efd53409756d
David Gregorc <davidgregorc@Davids-MacBook-Air.local>
2025-04-11 12:21:41 +0200

    dodan cikel glasba mladih

--- models/koncert.js
+++ models/koncert.js
@@ -15,7 +15,7 @@ const KoncertSchema = new mongoose.Schema({
         ime: { type: String, required: false }, // Ime izvajalca
         instrument: { type: String, required: false }, // Instrument izvajalca
     }], // ARRAY objektov, kjer vsak objekt vsebuje 'ime' in 'instrument'
-    cikel: { type: String, enum: ["mlada klasika", "mlada kreativa", "abonma", "gostuje"], required: true },
+    cikel: { type: String, enum: ["mlada klasika", "mlada kreativa", "glasbaMladih", "gostuje"], required: true },
 });
 
 module.exports = mongoose.model("Koncert", KoncertSchema);
